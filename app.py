bc949ebac07343dc2de3d2c909f61bd8641dc3a8
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2021-11-17 22:18:59 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Laboratório DevOps - FIAP 8ASO"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
 if __name__ == '__main__':
     app.run()
