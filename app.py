37d098c304502932724cd3e9795fa8b14af7d971
zarantonello <44817603+zarantonello@users.noreply.github.com>
2022-08-13 11:10:47 -0300

    Ajustes nos textos de teste unitário

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps | Wilson"
+    return "Laboratório DevOps"
 
 if __name__ == '__main__':
     app.run()
