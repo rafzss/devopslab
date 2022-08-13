84e94337f3d0f74343f8e585c426e0b418930fe1
zarantonello <44817603+zarantonello@users.noreply.github.com>
2022-08-13 09:55:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO"
+    return "Laboratório DevOps"
 
 if __name__ == '__main__':
     app.run()
