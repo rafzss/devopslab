804cc34a7a049ffaf9badd3c560a324d2dc4ee9d
zarantonello <44817603+zarantonello@users.noreply.github.com>
2022-09-17 17:22:32 -0300

    Updated text

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps | Wilson | v0.2"
+    return "Solution Sprint 5 - 8ASO"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
